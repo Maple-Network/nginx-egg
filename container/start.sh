d103851db4a49449c5b1bf1cbf30a33cb6606e1d
Oliver Wuthrich-Giroux <oliver@maplenetwork.ca>
2025-07-16 21:16:09 -0400

    Fix start.sh not working on php versions other than 8

--- container/start.sh
+++ container/start.sh
@@ -2,7 +2,14 @@
 rm -rf /home/container/tmp/*
 
 echo "⟳ Starting PHP-FPM..."
-/usr/sbin/php-fpm8 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
+PHP_FPM_BINARY=$(find /usr/sbin/ -name 'php-fpm*' -type f | sort -V | tail -n 1)
+
+if [ -z "$PHP_FPM_BINARY" ]; then
+    echo "✗ ERROR: No PHP-FPM binary found. Exiting."
+    exit 1
+fi
+
+$PHP_FPM_BINARY --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
 
 echo "⟳ Starting Nginx..."
 echo "✓ Successfully started"
